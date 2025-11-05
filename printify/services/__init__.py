2fcdf3f1fa5ebf2d3426935154679da75e5c7d6e
aahlijia <aahlijia@gmail.com>
2025-11-05 06:08:27 -0500

    feat: order service

--- printify/services/__init__.py
+++ printify/services/__init__.py
@@ -7,8 +7,10 @@ and orchestrate API calls for different Printify resources.
 
 from printify.services.product_service import ProductService
 from printify.services.shipping_service import ShippingService
+from printify.services.order_service import OrderService
 
 __all__ = [
     'ProductService',
     'ShippingService',
+    'OrderService',
 ]
